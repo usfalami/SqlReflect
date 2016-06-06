b796dc137e38313729bffc9daf8e02341dfe130f
YAH <usf.alami@gmail.com>
2016-06-06 17:18:59 +0200

    evol

--- src/usf/java/reflect/executor/adapter/ExecutorAdapter.java
+++ src/usf/java/reflect/executor/adapter/ExecutorAdapter.java
@@ -19,28 +19,23 @@ public abstract class ExecutorAdapter extends AbstractAdapter {
 	}
 	
 	public void execute(String query, Serializable... parameters) throws SQLException {
-		this.execute(1, query, parameters);
-	}
-	public void execute(String... queries) throws SQLException {
-		this.execute(1, queries);
-	}
-	public void execute(int times, String query, Serializable... parameters) throws SQLException {
 		Executor e = executorFor(parameters);
 		SQL sql = cm.parseSQL(query);
-		for(int i=0; i<times; i++) 
+		if(sql != null)
 			e.run(this, sql, parameters);
 	}
-	public void execute(int times, String... queries) throws SQLException {
+	public void execute(String... queries) throws SQLException { // only statment
 		if(queries == null) return;
 		Executor e = new StatmentExecutor();
-		for(int i=0; i<times; i++){
-			for(String query : queries) {
-				SQL sql = cm.parseSQL(query);
-				e.run(this, sql);
-			}
-		}
+		for(String query : queries)
+			e.run(this, cm.parseSQL(query));
 	}
-	
+	public void execute(String[] queries, Serializable[][] parameters) throws SQLException {
+		if(queries == null || parameters==null || queries.length != parameters.length) return; //throw exception
+		for(int i=0; i<queries.length; i++)
+			this.execute(queries[i], parameters[i]);
+	}
+
 	public abstract void preConnecion();
 	public abstract void postConnecion();
 	
