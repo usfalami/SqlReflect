97f9d73ed711d35a1c1055e1a2184f904a7fc1bb
YAH <usf.alami@gmail.com>
2016-06-06 10:10:49 +0200

    clean

--- src/usf/java/Main.java
+++ src/usf/java/Main.java
@@ -38,38 +38,17 @@ public class Main {
 	static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 	
 	static String query = Queries.cap1_Bind;
-	
 	static Serializable[] param=null;
-	
-	
-	static Serializable[][] par= null;
-	
-	static String[] it = {
-		"exec STM_IHM_pf1.MACR_CONS_MESR_C1C4_PDC_PARL_SES ( '2013-01-01' , '2015-01-01' , '19090984' , 30 , 'CONS' , 'EA' , 'COMPB' , '2014-01-01' , '2015-01-01' )",
-		"exec STM_IHM_pf1.MACR_CONS_MESR_C1C4_PDC_PARL_SES ( '2013-02-02' , '2015-02-02' , '19090984' , 30 , 'CONS' , 'EA' , 'COMPB' , '2014-02-02' , '2015-02-02' )",
-		"exec STM_IHM_pf1.MACR_CONS_MESR_C1C4_PDC_PARL_SES ( '2013-03-03' , '2015-03-03' , '19090984' , 30 , 'CONS' , 'EA' , 'COMPB' , '2014-03-03' , '2015-03-03' )",
-		"exec STM_IHM_pf1.MACR_CONS_MESR_C1C4_PDC_PARL_SES ( '2013-04-04' , '2015-04-04' , '19090984' , 30 , 'CONS' , 'EA' , 'COMPB' , '2014-04-04' , '2015-04-04' )",
-		"exec STM_IHM_pf1.MACR_CONS_MESR_C1C4_PDC_PARL_SES ( '2013-05-05' , '2015-05-05' , '19090984' , 30 , 'CONS' , 'EA' , 'COMPB' , '2014-05-05' , '2015-05-05' )",
-		"exec STM_IHM_pf1.MACR_CONS_MESR_C1C4_PDC_PARL_SES ( '2013-06-06' , '2015-06-06' , '19090984' , 30 , 'CONS' , 'EA' , 'COMPB' , '2014-06-06' , '2015-06-06' )",
-		"exec STM_IHM_pf1.MACR_CONS_MESR_C1C4_PDC_PARL_SES ( '2013-07-07' , '2015-07-07' , '19090984' , 30 , 'CONS' , 'EA' , 'COMPB' , '2014-07-07' , '2015-07-07' )"
-	};
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException, ParseException, FileNotFoundException {
 		
-//			query = Queries.cap1_Bind;
-//		param = new Serializable[]{
-//				 new Date(sdf.parse("2016-05-27").getTime()),
-//				 new Date(sdf.parse("2016-05-29").getTime()),
-//				 "09781620756483"};
+		query = Queries.cap1_Bind;
+		param = new Serializable[]{
+				 new Date(sdf.parse("2016-05-27").getTime()),
+				 new Date(sdf.parse("2016-05-29").getTime()),
+				 "09781620756483"};
 		
-		par    = new Serializable[6][];
-		par[0] = new Serializable[]{new Date(sdf.parse("2016-05-27").getTime()),new Date(sdf.parse("2016-05-29").getTime()), "09781620756483"};
-		par[1] = new Serializable[]{new Date(sdf.parse("2013-02-15").getTime()),new Date(sdf.parse("2014-01-23").getTime()), "25482778573169"};
-		par[2] = new Serializable[]{new Date(sdf.parse("2015-03-06").getTime()),new Date(sdf.parse("2016-05-29").getTime()), "25153256114306"};
-		par[3] = new Serializable[]{new Date(sdf.parse("2014-05-11").getTime()),new Date(sdf.parse("2015-03-06").getTime()), "16185817549601"};
-		par[4] = new Serializable[]{new Date(sdf.parse("2015-04-26").getTime()),new Date(sdf.parse("2016-05-29").getTime()), "5459768446848"};
-		par[5] = new Serializable[]{new Date(sdf.parse("2014-01-23").getTime()),new Date(sdf.parse("2015-02-15").getTime()), "23102460063230"};
-
+//		query = Queries.cap2_Bind;
 //		query = Queries.cap3_Bind;
 //		param = new Serializable[]{
 //				 new Date(sdf.parse("2013-07-01").getTime()),
@@ -86,8 +65,8 @@ public class Main {
 		System.out.println(query);
 		
 		test1();
-//		test2(); 
-//		test3();
+		test2(); 
+		test3();
 //		ex3();
 //		ex1();
 //		ex2();
@@ -96,7 +75,7 @@ public class Main {
 	//Excecutors & Adapters
 	public static void test1() throws InstantiationException, IllegalAccessException, SQLException, ParseException{
 		ExecutorAdapter a = new ExecutorPerformAdapter(factory, format);
-		a.execute(query);
+		a.execute(query, param);
 	}
 	public static void test2() throws InstantiationException, IllegalAccessException, SQLException, ParseException{
 		ExecutorAdapter a = new ExecutorColumnAdapter(factory, format);
