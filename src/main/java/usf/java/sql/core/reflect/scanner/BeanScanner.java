2b40f105ffec30dcd2f0e7e80a7c43cd2e607ab1
YAH <youssef-externe.alami@erdf.fr>
2016-06-21 14:53:16 +0200

    evol

--- src/main/java/usf/java/sql/core/reflect/scanner/BeanScanner.java
+++ src/main/java/usf/java/sql/core/reflect/scanner/BeanScanner.java
@@ -12,7 +12,7 @@ import usf.java.sql.core.field.Callable;
 import usf.java.sql.core.reflect.Reflector;
 
 public class BeanScanner extends Reflector implements Scanner {
-	
+
 	public BeanScanner(ConnectionManager cm) {
 		super(cm);
 	}
@@ -24,25 +24,7 @@ public class BeanScanner extends Reflector implements Scanner {
 			Statement stmt = null;
 			try {
 				stmt = cm.buildStatement(cnx, callable.getSQL(), parametters);
-				adapter.start();
-				ResultSet rs = null;
-				try {
-					rs = stmt instanceof Statement ? 
-							stmt.executeQuery(callable.getSQL()) : ((PreparedStatement)stmt).executeQuery();
-					int row = 0;
-					while(rs.next()) {
-						T bean = adapter.getMapper().map(rs, row+1);
-						adapter.adapte(bean, row++);
-					}
-					rs.beforeFirst();
-				} catch (SQLException e) {
-					e.printStackTrace();
-					throw e;
-				}
-				finally {
-					cm.close(rs);
-					adapter.end();
-				}
+				run(stmt, adapter, callable, parametters);
 			} catch (SQLException e) {
 				e.printStackTrace();
 				throw e;
@@ -50,13 +32,33 @@ public class BeanScanner extends Reflector implements Scanner {
 			finally {
 				cm.close(stmt);
 			}
-		
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw e;
 		}
 		finally {
 			cm.close(cnx);
+		}
+	}
+
+	protected <T> void run(Statement stmt, HasScanner<T> adapter, Callable callable, Serializable ... parametters) throws SQLException {
+		adapter.start();
+		ResultSet rs = null;
+		try {
+			rs = stmt instanceof Statement ? stmt.executeQuery(callable.getSQL()) : ((PreparedStatement)stmt).executeQuery();
+			int row = 0;
+			while(rs.next()) {
+				T bean = adapter.getMapper().map(rs, row+1);
+				adapter.adapte(bean, row++);
+			}
+			rs.beforeFirst();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		}
+		finally {
+			cm.close(rs);
+			adapter.end();
 		}
 	}
 
