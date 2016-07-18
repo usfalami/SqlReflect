f9990f1340148e2dcf16d96de42b2ad0fefa69a2
YAH <youssef-externe.alami@erdf.fr>
2016-07-18 12:15:37 +0200

    evol

--- src/main/java/usf/java/sql/core/adapter/TimePerformAdapter.java
+++ src/main/java/usf/java/sql/core/adapter/TimePerformAdapter.java
@@ -3,13 +3,11 @@ package usf.java.sql.core.adapter;
 import java.sql.SQLException;
 
 import usf.java.sql.core.exception.AdapterException;
-import usf.java.sql.core.field.Query;
 import usf.java.sql.core.field.TimePerform;
 import usf.java.sql.core.reflect.performer.Performer.PerformAdapter;
 
 public class TimePerformAdapter implements PerformAdapter {
 
-	protected Query callable;
 	protected TimePerform time;
 	
 	@Override
