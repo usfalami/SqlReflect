2856dbe8bbba6c76a8f1aabb681876c61f8e9cce
YAH <youssef-externe.alami@erdf.fr>
2016-09-09 17:35:01 +0200

    evol

---
+++ src/main/java/usf/java/sqlreflect/connection/transaction/SqlUtils.java
@@ -0,0 +1,38 @@
+package usf.java.sqlreflect.connection.transaction;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import usf.java.sqlreflect.field.Parameter;
+
+public class SqlUtils {
+	
+	public static void bindPreparedStatement(PreparedStatement pstmt, Parameter<?>... args) throws SQLException {
+		if(args == null) return;
+		for(int i=0; i<args.length; i++)
+			pstmt.setObject(i+1, args[i].getValue());
+	}
+	
+	public static void bindCallableStatement(CallableStatement cstmt, Parameter<?>... args) throws SQLException {
+		if(args == null) return;
+		for(int i=0; i<args.length; i++){
+			Parameter<?> arg = args[i];
+			if(arg.isOut())
+				cstmt.registerOutParameter(i+1, arg.getSqlType());
+			else
+				cstmt.setObject(i+1, arg.getValue());
+		}
+	}
+	
+	public static void updateOutParameter(CallableStatement cstmt, Parameter<?>... args) throws SQLException {
+		if(args == null) return;
+		for(int i=0; i<args.length; i++){
+			Parameter arg = args[i];
+			if(arg.isOut())
+				arg.setValue(cstmt.getObject(i+1));
+		}
+	}
+
+
+}
