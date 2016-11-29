76afaa1adef8bc0aae33e978e644158cc334dcfd
uSf <usf.alami@gmail.com>
2016-11-29 20:49:50 +0100

    evol

--- src/main/java/usf/java/sqlreflect/stream/JsonStreamWriter.java
+++ src/main/java/usf/java/sqlreflect/stream/JsonStreamWriter.java
@@ -10,6 +10,8 @@ public class JsonStreamWriter implements StreamWriter {
 	
 	protected JSONWriter jwriter;
 	
+	int level = 0;
+	
 	public JsonStreamWriter(Writer writer) {
 		jwriter = new JSONWriter(writer); 
 	}
@@ -17,59 +19,82 @@ public class JsonStreamWriter implements StreamWriter {
 	@Override
 	public void writeBoolean(String name, boolean bool) throws JSONException {
 		jwriter.key(name).value(bool);
+		debug(level+1, "value");
 	}
 
 	@Override
 	public void writeInt(String name, int number) throws JSONException {
 		jwriter.key(name).value(number);
+		debug(level+1, "value");
 	}
 
 	@Override
 	public void writeLong(String name, long number) throws JSONException {
 		jwriter.key(name).value(number);
+		debug(level+1, "value");
 	}
 
 	@Override
 	public void writeFloat(String name, float number) throws JSONException {
 		jwriter.key(name).value(number);
+		debug(level+1, "value");
 	}
 
 	@Override
 	public void writeDouble(String name, double number) throws JSONException {
 		jwriter.key(name).value(number);
+		debug(level+1, "value");
 	}
 
 	@Override
 	public void writeString(String name, String string) throws JSONException {
 		jwriter.key(name).value(string);
+		debug(level+1, "value");
 	}
 
 	@Override
 	public void writeDate(String name, Date date) throws JSONException {
 		jwriter.key(name).value(DATE_FORMATTER.format(date));
+		debug(level+1, "value");
 	}
 
 	@Override
 	public void startObject(String name) throws JSONException {
 		jwriter.object();
+		debug(++level, "object");
 	}
 	@Override
 	public void endObject() throws JSONException {
 		jwriter.endObject();
+		debug(level--, "endObject");
 	}
 	
 	@Override
 	public void startList(String name, String... columns) throws Exception {
+		jwriter.key(name);
 		jwriter.array();
+		debug(++level, "array");
 	}
 	@Override
 	public void endList() throws Exception {
 		jwriter.endArray();
+		debug(level--, "endArray");
 	}
 	
 	@Override
-	public void start() throws Exception {}
+	public void start() throws Exception {
+		jwriter.array();
+		debug(0, "array");
+	}
 	@Override
-	public void end() throws Exception {}
+	public void end() throws Exception {
+		jwriter.endArray();
+		debug(0, "endArray");
+	}
+	
+	private void debug(int level, String text) {
+		if(level == 0) System.out.println(text);
+		else System.out.println(String.format("%" + level + "s", "").replace(" ", "|......") + text);
+	}
 	
 }
--- src/test/java/usf/java/sqlreflect/ContextLoader.java
+++ src/test/java/usf/java/sqlreflect/ContextLoader.java
@@ -30,6 +30,7 @@ import usf.java.sqlreflect.sql.entry.PrimaryKey;
 import usf.java.sqlreflect.sql.entry.Procedure;
 import usf.java.sqlreflect.sql.entry.Table;
 import usf.java.sqlreflect.sql.type.TableTypes;
+import usf.java.sqlreflect.stream.JsonStreamWriter;
 import usf.java.sqlreflect.stream.PrinterStreamWriter;
 import usf.java.sqlreflect.stream.StreamWriter;
 import usf.java.sqlreflect.stream.XmlStreamWriter;
@@ -113,19 +114,20 @@ public class ContextLoader {
 		ConnectionManager cm = getConnectionManager();
 		StringWriter c = new StringWriter();
 //		StreamWriter ps = new XmlStreamWriter(c);
-		StreamWriter ps = new PrinterStreamWriter(System.out);
+		StreamWriter ps = new JsonStreamWriter(c);
+//		StreamWriter ps = new PrinterStreamWriter(System.out);
 		ps.start();
 		
 		new DatabaseScanner(cm).run(new FullWriter<Database>(ps));
 		new TableScanner(cm).set("mysql", "time_zone%").run(new FullWriter<Table>(ps));
-		new TableScanner(cm).set("sys", "%io", false, TableTypes.VIEW).run(new FullWriter<Table>(ps));
-		new ProcedureScanner(cm).set("sys", "%show%").run(new FullWriter<Procedure>(ps));
-		new HeaderScanner<Void>(cm).set("show processlist").run(new FullWriter<Header>(ps));
-		new PrimaryKeyScanner(cm).set(null, "country").run(new FullWriter<PrimaryKey>(ps));
-
-		RowScanner<Void, Entry> rs = new RowScanner<Void, Entry>(cm, new EntryMapper<Entry>(Entry.class));
-		rs.set("SELECT * FROM country WHERE name like 'MA%'").run(new FullWriter<Entry>(ps));
-		
+//		new TableScanner(cm).set("sys", "%io", false, TableTypes.VIEW).run(new FullWriter<Table>(ps));
+//		new ProcedureScanner(cm).set("sys", "%show%").run(new FullWriter<Procedure>(ps));
+//		new HeaderScanner<Void>(cm).set("show processlist").run(new FullWriter<Header>(ps));
+//		new PrimaryKeyScanner(cm).set(null, "country").run(new FullWriter<PrimaryKey>(ps));
+//
+//		RowScanner<Void, Entry> rs = new RowScanner<Void, Entry>(cm, new EntryMapper<Entry>(Entry.class));
+//		rs.set("SELECT * FROM country WHERE name like 'MA%'").run(new FullWriter<Entry>(ps));
+//		
 		ps.end();
 		System.out.println(c);
 		forceCloseConnectionManager();
